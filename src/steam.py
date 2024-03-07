07da44717f730ae37440a597a9f234884b3f348a
liph <lipenghua@lipenghua.com>
2024-03-07 17:21:42 +0800

    update

--- src/steam.py
+++ src/steam.py
@@ -1,41 +1,65 @@
 from common import *
+import requests
 import sys
 
 
+# https://developer.valvesoftware.com/wiki/Steam_Web_API
+# https://wiki.teamfortress.com/wiki/User:RJackson/StorefrontAPI
 class SteamAPI:
     def __init__(self, key, steamid):
         self.key = key
         self.steamid = steamid
+        self.session = requests.Session()
+        self.requests_count = 0
 
-    def GetOwnedGames(self):
-        url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={self.key}&steamid={self.steamid}&format=json'
-        r = fetch_url(url)
+    def __del__(self):
+        eprint('requests_count:', self.requests_count)
+
+    def fetch(self, url, try_times=5, sleep_interval=1):
+        if try_times == 0:
+            return None
+        try:
+            self.requests_count += 1
+            return self.session.get(url)
+        except Exception:
+            time.sleep(sleep_interval)
+            return self.fetch(url, try_times=try_times - 1, sleep_interval=sleep_interval)
+
+    def GetOwnedGames(self, steamid=None):
+        if steamid is None:
+            steamid = self.steamid
+        url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={self.key}&steamid={steamid}&format=json&skip_unvetted_apps=0&include_appinfo=1&include_played_free_games=1&include_free_sub=0'
+        r = self.fetch(url)
         return r.json()
 
     def GetPlayerAchievements(self, appid):
         url = f'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={appid}&key={self.key}&steamid={self.steamid}'
-        r = fetch_url(url)
+        r = self.fetch(url)
         return r.json()
 
     def GetPlayerSummaries(self):
         url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={self.key}&steamids={self.steamid}'
-        r = fetch_url(url)
+        r = self.fetch(url)
         return r.json()
 
     def GetFriendList(self):
         url = f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={self.key}&steamid={self.steamid}&relationship=friend'
-        r = fetch_url(url)
+        r = self.fetch(url)
         return r.json()
 
     def GetUserStatsForGame(self, appid):
         url = f'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={appid}&key={self.key}&steamid={self.steamid}'
-        r = fetch_url(url)
+        r = self.fetch(url)
         return r.json()
 
     def GetRecentlyPlayedGames(self):
         url = f'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={self.key}&steamid={self.steamid}&format=json'
-        r = fetch_url(url)
+        r = self.fetch(url)
         return r.json()
+
+    def GetAppDetails(self, appid):
+        url = f'https://store.steampowered.com/api/appdetails?appids={appid}'
+        return self.fetch(url).json()
 
 
 def main(key, steamid):
@@ -47,13 +71,17 @@ def main(key, steamid):
     output = []
     games = api.GetOwnedGames()['response']
     for game in games['games']:
+        appid = str(game['appid'])
         item = {}
-        item['appid'] = game['appid']
-        Achievements = api.GetPlayerAchievements(game['appid'])['playerstats']
-        if ('error' in Achievements):
-            output.append(item)
+        item['appid'] = appid
+        achievements = api.GetPlayerAchievements(appid)['playerstats']
+        if ('error' in achievements):
+            details = api.GetAppDetails(appid)
+            if details[appid]['success']:
+                item['name'] = details[appid]['data']['name']
+                output.append(item)
             continue
-        item['name'] = Achievements['gameName']
+        item['name'] = achievements['gameName']
         if 'rtime_last_played' in game and game['rtime_last_played'] != 0:
             item['last_time'] = format_time(game['rtime_last_played'])
         item['total_time'] = game['playtime_forever']
